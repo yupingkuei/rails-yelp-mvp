9db6a8755444d1f9419f4d4e90910f4bf87c795a
Yuping Kuei <kueiyuping@gmail.com>
2020-11-04 13:05:14 +0900

    Finished the retaurants controller and the index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating 10 fake restaurants...'
+10.times do
+  Restaurant.create!(
+    name: Faker::Creature::Animal.name,
+    address: Faker::Address.country,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: Restaurant::CATEGORIES.sample
+  )
+end
+puts 'Finished!'
